adb43ff81fe9a20efc50ed716350f0dec7c1784e
phamdat1992 <dathtp@gmail.com>
2020-10-30 11:16:36 +0700

    fix detect rectangle

--- recCorners.h
+++ recCorners.h
@@ -8,6 +8,23 @@ using namespace cv;
 vector<vector<Point> > contours;
 vector<Vec4i> hierachy;
 
+double kc(double x1, double y1, double x2, double y2)
+{
+	double dx = x1 - x2;
+	double dy = y1 - y2;
+	return sqrt(dx * dx + dy * dy);
+}
+
+double sTriangle(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+	double c1 = kc(x1, y1, x2, y2);
+	double c2 = kc(x1, y1, x3, y3);
+	double c3 = kc(x2, y2, x3, y3);
+
+	double p = (c1 + c2 + c3) / 2;
+	return sqrt(p * (p - c1) * (p - c2) * (p - c3));
+}
+
 Mat recCorners(Mat m, Mat im_src)
 {
 	Mat mThresh;
@@ -25,7 +42,8 @@ Mat recCorners(Mat m, Mat im_src)
 
 	int midX = 0;
 	int midY = 0;
-	for (int i = 0; i < contours[0].size(); ++i) {
+	for (int i = 0; i < contours[0].size(); ++i)
+	{
 		midX += contours[0][i].x;
 		midY += contours[0][i].y;
 	}
@@ -33,16 +51,49 @@ Mat recCorners(Mat m, Mat im_src)
 	midX /= contours[0].size();
 	midY /= contours[0].size();
 
+	Point2f top(0, m.rows), bottom(0, 0), left(m.cols, 0), right(0, 0);
+	for (int i = 0; i < contours[0].size(); ++i)
+	{
+		if (contours[0][i].x == midX)
+		{
+			if (top.y > contours[0][i].y)
+			{
+				top.x = contours[0][i].x;
+				top.y = contours[0][i].y;
+			}
+			if (bottom.y < contours[0][i].y)
+			{
+				bottom.x = contours[0][i].x;
+				bottom.y = contours[0][i].y;
+			}
+		}
+
+		if (contours[0][i].y == midY)
+		{
+			if (left.x > contours[0][i].x)
+			{
+				left.x = contours[0][i].x;
+				left.y = contours[0][i].y;
+			}
+			if (right.x < contours[0][i].x)
+			{
+				right.x = contours[0][i].x;
+				right.y = contours[0][i].y;
+			}
+		}
+	}
+
 	vector<Point2f> pts_src;
 	double ddx = 0.0;
 	int xl, yl;
+	// dinh tren ben phai hcn
 	for (int i = 0; i < contours[0].size(); ++i)
 	{
 		if (contours[0][i].x > midX && contours[0][i].y < midY)
 		{
-			double dxl = (double)contours[0][i].x - (double)midX;
-			double dyl = (double)contours[0][i].y - (double)midY;
-			double dcm = sqrt(dxl * dxl + dyl * dyl);
+			double s1 = sTriangle(midX, midY, top.x, top.y, contours[0][i].x, contours[0][i].y);
+			double s2 = sTriangle(midX, midY, right.x, right.y, contours[0][i].x, contours[0][i].y);
+			double dcm = s1 + s2;
 			if (dcm > ddx) {
 				ddx = dcm;
 				xl = contours[0][i].x;
@@ -53,13 +104,14 @@ Mat recCorners(Mat m, Mat im_src)
 	pts_src.push_back(Point2f(xl, yl));
 
 	ddx = 0.0;
+	// dinh tren ben trai hcn
 	for (int i = 0; i < contours[0].size(); ++i)
 	{
 		if (contours[0][i].x < midX && contours[0][i].y < midY)
 		{
-			double dxl = (double)contours[0][i].x - (double)midX;
-			double dyl = (double)contours[0][i].y - (double)midY;
-			double dcm = sqrt(dxl * dxl + dyl * dyl);
+			double s1 = sTriangle(midX, midY, top.x, top.y, contours[0][i].x, contours[0][i].y);
+			double s2 = sTriangle(midX, midY, left.x, left.y, contours[0][i].x, contours[0][i].y);
+			double dcm = s1 + s2;
 			if (dcm > ddx) {
 				ddx = dcm;
 				xl = contours[0][i].x;
@@ -70,13 +122,14 @@ Mat recCorners(Mat m, Mat im_src)
 	pts_src.push_back(Point2f(xl, yl));
 
 	ddx = 0.0;
+	// dinh duoi ben trai hcn
 	for (int i = 0; i < contours[0].size(); ++i)
 	{
 		if (contours[0][i].x < midX && contours[0][i].y > midY)
 		{
-			double dxl = (double)contours[0][i].x - (double)midX;
-			double dyl = (double)contours[0][i].y - (double)midY;
-			double dcm = sqrt(dxl * dxl + dyl * dyl);
+			double s1 = sTriangle(midX, midY, bottom.x, bottom.y, contours[0][i].x, contours[0][i].y);
+			double s2 = sTriangle(midX, midY, left.x, left.y, contours[0][i].x, contours[0][i].y);
+			double dcm = s1 + s2;
 			if (dcm > ddx) {
 				ddx = dcm;
 				xl = contours[0][i].x;
@@ -87,13 +140,14 @@ Mat recCorners(Mat m, Mat im_src)
 	pts_src.push_back(Point2f(xl, yl));
 
 	ddx = 0.0;
+	// dinh duoi ben phai hcn
 	for (int i = 0; i < contours[0].size(); ++i)
 	{
 		if (contours[0][i].x > midX && contours[0][i].y > midY)
 		{
-			double dxl = (double)contours[0][i].x - (double)midX;
-			double dyl = (double)contours[0][i].y - (double)midY;
-			double dcm = sqrt(dxl * dxl + dyl * dyl);
+			double s1 = sTriangle(midX, midY, bottom.x, bottom.y, contours[0][i].x, contours[0][i].y);
+			double s2 = sTriangle(midX, midY, right.x, right.y, contours[0][i].x, contours[0][i].y);
+			double dcm = s1 + s2;
 			if (dcm > ddx) {
 				ddx = dcm;
 				xl = contours[0][i].x;
